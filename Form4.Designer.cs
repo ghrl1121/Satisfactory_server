1624584d81b6c85f699038dad2aac7a2486580b5
호기 김 <ghrl1005@naver.com>
2025-10-17 22:15:51 +0900

    반복 작업 추가 일시정지 및 다시시작 추가

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -43,6 +43,9 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label9 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // checkBox1
@@ -177,12 +180,45 @@
             this.label9.TabIndex = 17;
             this.label9.Text = "저장위치 : ";
             // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(12, 300);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "일시정지";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(93, 300);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "다시시작";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 270);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 20;
+            // 
             // Form4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(350, 335);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.linkLabel2);
@@ -223,5 +259,8 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Form4.cs
+++ Form4.cs
@@ -1,28 +1,38 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Drawing.Text;
 using System.IO;
+using System.Reflection.Emit;
 using System.Security.Permissions;
+using System.Threading;
 using System.Timers;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
 
 namespace Satisfactory_서버용
 {
     public partial class Form4 : Form
     {
 
-
+        private Thread thd;
+        delegate void CrossCall();
         public Form4()
         {
-
             InitializeComponent();
             //실행 채크
-            var unme = Process.GetProcessesByName("FactoryServer-Win64-Shipping-Cmd");
-            if (unme.Length > 0)
+            //반복으로 확인하기
+            int i = 0;
+            while (i < 2)
             {
-                label8.Text = "실행중입니다. \n\r포트를 바꿔주세요 오류 납니다!\n\r옆 중지 누르면 서버중지(전부)됩니다";
-                linkLabel2.Text = "중지";
+                i++;
+                var unme = Process.GetProcessesByName("FactoryServer-Win64-Shipping-Cmd");
+                if (unme.Length > 0)
+                {
+                    label8.Text = "실행중입니다. \n\r포트를 바꿔주세요 오류 납니다!\n\r옆 중지 누르면 서버중지(전부)됩니다";
+                    linkLabel2.Text = "중지";
+                }
             }
-
         }
         public Form4(string data)
         {
@@ -66,59 +76,94 @@ namespace Satisfactory_서버용
                 }
                 else
                 {
-                    if (checkBox1.Checked == true)
+                    thd = new Thread(new ThreadStart(ThreadFunction));
+                    label2.Text = "실행중...";
+                    button1.Enabled = false;
+                    button2.Enabled = true;
+                    thd.Start();
+                }
+                   
+            }
+        }
+
+        private void ThreadFunction()
+        {
+            //포트 쿼리
+            int Query = int.Parse(textBox1.Text);
+            //포트 게임
+            int Port = int.Parse(textBox3.Text);
+            if (checkBox1.Checked == true)
+            {
+                while (true)
+                {
+                    var unpe = Process.GetProcessesByName("FactoryServer-Win64-Shipping-Cmd");
+                    if (unpe.Length > 0)
                     {
-                    A:
-                        var unpe = Process.GetProcessesByName("FactoryServer-Win64-Shipping-Cmd");
-                        if (unpe.Length > 0)
-                        {
-                            //실행중...
-                            //대기시간
-                        }
-                        else
-                        {
-                            //다시시작
-                            /*var nem = new ProcessStartInfo(textBox2.Text + @"\FactoryServer.exe", " -ServerQusryPort=" + Query + " -port=" + Port + " -unattended" + " -NoAsyncLoadingThread -UseMultithreadForDS -log");
-                            nem.UseShellExecute = false;
-                            Process.Start(nem);*/
-                            Process process = new Process();
-                            process.StartInfo.FileName = "FactoryServer.exe";
-                            process.StartInfo.Arguments = " -ServerQusryPort=" + Query + " -port=" + Port + " -unattended" + " -NoAsyncLoadingThread -UseMultithreadForDS -log";
-                            process.StartInfo.WorkingDirectory = textBox2.Text;
-                            process.Start();
-                            process.WaitForExit(5000);
-                            //대기시간
-                        }
-                        goto A;
+                        //실행중...
+                        label2.Text = "실행중...";
+                        //대기시간
+                        Thread.Sleep(3000);
                     }
                     else
                     {
-                    A:
-                        var unpe = Process.GetProcessesByName("FactoryServer-Win64-Shipping-Cmd");
-                        if (unpe.Length > 0)
-                        {
-                            //실행중
-                            //대기시간
-                        }
-                        else
-                        {
-                            //없음
-                            /*var nem = new ProcessStartInfo(textBox2.Text + @"\FactoryServer.exe", " -ServerQusryPort=" + Query + " -port=" + Port + " -NoAsyncLoadingThread -UseMultithreadForDS -log");
-                            nem.UseShellExecute = false;
-                            Process.Start(nem);*/
-                            //대기시간
-                            Process process = new Process();
-                            process.StartInfo.FileName = "FactoryServer.exe";
-                            process.StartInfo.Arguments = " -ServerQusryPort=" + Query + " -port=" + Port + " -NoAsyncLoadingThread -UseMultithreadForDS -log";
-                            process.StartInfo.WorkingDirectory = textBox2.Text;
-                            process.Start();
-                            process.WaitForExit(5000);
-
-                        }
-                        goto A;
+                        //다시시작
+                        /*var nem = new ProcessStartInfo(textBox2.Text + @"\FactoryServer.exe", " -ServerQusryPort=" + Query + " -port=" + Port + " -unattended" + " -NoAsyncLoadingThread -UseMultithreadForDS -log");
+                        nem.UseShellExecute = false;
+                        Process.Start(nem);*/
+                        Process process = new Process();
+                        process.StartInfo.FileName = "FactoryServer.exe";
+                        process.StartInfo.Arguments = " -ServerQusryPort=" + Query + " -port=" + Port + " -unattended" + " -NoAsyncLoadingThread -UseMultithreadForDS -log";
+                        process.StartInfo.WorkingDirectory = textBox2.Text;
+                        process.Start();
+                        //대기시간
+                        process.WaitForExit(5000);
                     }
                 }
             }
+            else
+            {
+                while (true)
+                {
+                    var unpe = Process.GetProcessesByName("FactoryServer-Win64-Shipping-Cmd");
+                    if (unpe.Length > 0)
+                    {
+                        //실행중
+                        
+                        //대기시간
+                        Thread.Sleep(3000);
+                    }
+                    else
+                    {
+                        //다시시작
+                        /*var nem = new ProcessStartInfo(textBox2.Text + @"\FactoryServer.exe", " -ServerQusryPort=" + Query + " -port=" + Port + " -NoAsyncLoadingThread -UseMultithreadForDS -log");
+                        nem.UseShellExecute = false;
+                        Process.Start(nem);*/
+                        Process process = new Process();
+                        process.StartInfo.FileName = "FactoryServer.exe";
+                        process.StartInfo.Arguments = " -ServerQusryPort=" + Query + " -port=" + Port + " -NoAsyncLoadingThread -UseMultithreadForDS -log";
+                        process.StartInfo.WorkingDirectory = textBox2.Text;
+                        process.Start();
+                        //대기시간
+                        process.WaitForExit(5000);
+                    }
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            thd.Suspend();
+            label2.Text = "일시정지";
+            button3.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            thd.Resume();
+            label2.Text = "다시시작";
+            label2.Text = "실행중...";
+            button2.Enabled = true;
+            button3.Enabled = false;
         }
     }
 }
